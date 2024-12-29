300288e8a804885e300cbfc05814bffd810cbeed
Xinhui Yang <cyan@cyano.uk>
2024-12-29 22:51:24 +0800

    devices: add generic/pc-efi
    This device is mostly for testing purposes, but it is usable.

---
+++ devices/generic/pc-efi/apply-bootloader.sh
@@ -0,0 +1,10 @@
+echo "Disabling os-prober ..."
+echo -e "\nGRUB_DISABLE_OS_PROBER=true" >> /etc/default/grub
+
+echo "Installing grub ..."
+grub-install --removable --efi-directory=/efi --target=x86_64-efi
+
+echo "Updating initrd ..."
+update-initramfs
+
+echo "Done!"
